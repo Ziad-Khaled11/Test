6727a1c4a5e95a4129afd2c6c25cc6165a48385b
Ziad Khaled <ziadshraf100@gmail.com>
2024-04-20 02:33:52 +0200

    'l'

---
+++ test/build/Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_test_FILE E:/Test/test/build/Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/test.exe)
+set(__QT_DEPLOY_TARGET_test_RUNTIME_DLLS C:/Qt/6.7.0/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.0/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.0/msvc2019_64/bin/Qt6Cored.dll)
