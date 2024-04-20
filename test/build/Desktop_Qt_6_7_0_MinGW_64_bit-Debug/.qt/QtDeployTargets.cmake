49f5e82e24ccd4a49fc78f4d84f68205a302ce00
roaa.lotfy <roaa.m.lotfy@gmail.com>
2024-04-20 02:18:45 +0200

    'new_button'

---
+++ test/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_test_FILE D:/lol/Test/test/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/test.exe)
+set(__QT_DEPLOY_TARGET_test_RUNTIME_DLLS D:/qt_cereator/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/qt_cereator/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/qt_cereator/6.7.0/mingw_64/bin/Qt6Core.dll)
